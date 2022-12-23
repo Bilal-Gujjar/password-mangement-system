f121cef55e793301b178780b48130d7dfd5506ea
bilal-gujjar <bilaltahir515@gmail.com>
2022-12-23 10:59:32 +0500

    Code cleaning

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PasswordManagerModule } from './password-manager/password-manager.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: '.env',
+        }),
+      ],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('HOST'),
+        port: configService.get<number>('PORT'),
+        username: configService.get('USERNAME'),
+        password: configService.get('PASSWORD'),
+        database: configService.get('DATABASE'),
+        synchronize: configService.get<boolean>('SYNC'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    PasswordManagerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/transform.intersecptor.ts
@@ -0,0 +1,14 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { classToPlain, instanceToPlain } from 'class-transformer';
+import { map } from 'rxjs/operators';
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler) {
+    return next.handle().pipe(map((data) => instanceToPlain(data)));
+  }
+}
